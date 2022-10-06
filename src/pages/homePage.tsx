fcf47b1d5616d1da4588e55620e889d3613482f1
lassehj0 <lassehj00@github.com>
2022-10-06 15:27:33 +0200

    added router buttons

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<>
-			<div id='hej'></div>
+			<div id='hej'>
+				<Link to='skills'>
+					<button id='skillsBtn'>Skills</button>
+				</Link>
+				<Link to='about'>
+					<button id='aboutBtn'>About me</button>
+				</Link>
+			</div>
 		</>
 	);
 };
