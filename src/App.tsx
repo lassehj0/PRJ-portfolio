5004a1af16744f2430a24c017f4fee046af01bd8
mjuulsgaard <mikkeljuulsgaard@live.dk>
2022-10-06 12:03:28 +0200

    nada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-	return <></>;
+  return (
+    <>
+      <p>Hello there</p>
+      <p>
+        <button onClick={clickAlert}>Click me</button>
+      </p>
+    </>
+  );
+}
+function clickAlert() {
+  alert("You clicked me!");
 }
 
 export default App;
