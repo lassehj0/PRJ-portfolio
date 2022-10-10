4ba3328b55f74fca594d06e6424f9bc29524994d
lassehj0 <lassehj00@github.com>
2022-10-10 13:46:58 +0200

    added gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 	}, []);
 
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
