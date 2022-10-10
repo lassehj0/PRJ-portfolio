a6a4d1ebf70b06921f7ec84b0b6df5d3d8d29768
lassehj0 <lassehj00@github.com>
2022-10-10 14:40:20 +0200

    added regions

--- src/pages/aboutPage.tsx
+++ src/pages/aboutPage.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import picture from '../nonbinary-person.png';
 
 const About = () => {
 	return (
 		<>
-			<div id='title'>About me</div>
-			<div id='description'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-				reprehenderit consectetur asperiores excepturi culpa pariatur, doloribus
-				voluptatum dignissimos sint alias quia molestiae omnis est ipsa odit,
-				neque quaerat sapiente tempora porro repellendus, dolor quasi dolorem
-				ipsum? Consectetur voluptatum beatae libero sunt maxime cupiditate optio
-				doloribus, iure obcaecati animi fuga explicabo?
-			</div>
-			<div id='list'>
-				<div id='ul'>
-					<li className='item1'></li>
-					<li className='item2'></li>
-					<li className='item3'></li>
-					<li className='item4'></li>
-					<li className='item5'></li>
+			<div id='about'>
+				<div id='title'>
+					<h1>About me</h1>
 				</div>
+				<div id='description'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+					reprehenderit consectetur asperiores excepturi culpa pariatur,
+					doloribus voluptatum dignissimos sint alias quia molestiae omnis est
+					ipsa odit, neque quaerat sapiente tempora porro repellendus, dolor
+					quasi dolorem ipsum? Consectetur voluptatum beatae libero sunt maxime
+					cupiditate optio doloribus, iure obcaecati animi fuga explicabo?
+				</div>
+				<div id='list'>
+					<div id='ul'>
+						<li className='item1'>1</li>
+						<li className='item2'>2</li>
+						<li className='item3'>3</li>
+						<li className='item4'>4</li>
+						<li className='item5'>5</li>
+					</div>
+				</div>
+				<div id='picture'>
+					<img id='pic' src={picture} alt='insertPersonHere' />
+				</div>
+				<div id='floats'></div>
 			</div>
-			<div id='picture'>
-				<img id='pic' src={picture} alt='insertPersonHere' />
-			</div>
-			<div id='floats'></div>
 		</>
 	);
 };
