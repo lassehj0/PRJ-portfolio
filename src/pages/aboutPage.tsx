f2af31eb15f7ec6d1fef1e61f7a377ed7e3e819c
mmmpedersen <202006450@post.au.dk>
2022-10-10 13:22:57 +0200

    idk

--- src/pages/aboutPage.tsx
+++ src/pages/aboutPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
 	return (
 		<>
-			<div id='hej'></div>
+			<div id='hej'>
+				<Link to='/'>
+					<button id='homePageBtn'>HomePage</button>
+				</Link>
+				<p>hej</p>
+			</div>
 		</>
 	);
 };
