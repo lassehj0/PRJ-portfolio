ba66d4dfe8a960d9127b9040c570994ca2b37779
Nadixo <75481929+Nadixo@users.noreply.github.com>
2022-10-18 12:17:57 +0200

    asdas

--- src/pages/skillsPage.tsx
+++ src/pages/skillsPage.tsx
@@ -1,17 +1,49 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./skillsPage.css";
 import "./sphere.tsx";
 //https://codepen.io/ryasan86/pen/bGpqdYV
 const Skills = () => {
+  const texts: string[] = [
+    "React",
+    "GraphQL",
+    "TypeScript",
+    "Prisma",
+    "Apollo",
+    "NodeJS",
+    "Express",
+    "JWT",
+    "Figma",
+    "Git",
+    "Netlify",
+    "Heroku",
+    "Angular",
+    "SSR",
+    "_Lodash",
+    "Python",
+    "Redux",
+    "REST",
+    "Cloudinary",
+    "Firebase",
+    "SCSS",
+    "TDD",
+    "Ionic",
+  ];
+  const axiosInstance = axios.create();
+  axiosInstance.get().then(Response);
+
   return (
     <>
-      <div id="hej">
+      <div id="skills">
         <Link to="/">
           <button id="homePageBtn">HomePage</button>
         </Link>
-        <p>hej</p>
+        <h1 id="title">Skills</h1>
+        <div>{JSON.stringify(string)}</div>
       </div>
+
       <div id="root"></div>
     </>
   );
