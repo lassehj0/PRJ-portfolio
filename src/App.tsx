dff9f905266a2d54075669aa8101678a74f20ecc
lassehj0 <lassehj00@github.com>
2022-10-31 14:32:48 +0100

    moved background scribble stuff to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import WebFont from 'webfontloader';
 import Home from './pages/homePage';
@@ -7,22 +8,121 @@ import About from './pages/aboutPage';
 import Skills from './pages/skillsPage';
 
 function App() {
+	var pos = [
+		[0.065, 0.135],
+		[0.805, 0.866],
+		[0.111, 0.731],
+		[0.823, 0.169],
+		[0.409, 0.135],
+		[0.368, 0.657],
+		[0.173, 0.467],
+		[0.269, 0.851],
+		[0.772, 0.537],
+		[0.555, 0.784],
+		[0.142, 0.176],
+		[0.577, 0.537],
+	];
+
+	var lang: string[] = [];
 	useEffect(() => {
 		WebFont.load({
 			google: {
 				families: ['La Belle Aurore'],
 			},
 		});
+
+		const axiosInstance = axios.create();
+		axiosInstance
+			.get('https://localhost:7062/api/Skills')
+			.then((response) => {
+				response.data.forEach((elem: any) => {
+					var p = document.createElement('p');
+					p.className = 'scribble';
+					p.id = elem.skillName;
+					p.innerHTML = elem.skillName;
+					p.style.marginLeft =
+						pos[elem.skillID - 1][0] * window.innerWidth + 'px';
+					p.style.marginTop =
+						pos[elem.skillID - 1][1] * window.innerHeight + 'px';
+					p.style.position = 'absolute';
+					document.getElementById('langs')?.appendChild(p);
+					lang.push(elem.skillName);
+				});
+			})
+			.catch(console.error);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	document.onmousemove = function (e) {
+		var x = e.clientX;
+		var y = e.clientY;
+		lang.forEach((entry, i) => {
+			var style = document.getElementById(entry)?.style;
+			if (style != null) {
+				style.marginLeft =
+					([1, 4, 5, 8, 10, 14, 15, 16, 19, 21].indexOf(i) !== -1
+						? pos[i][0] * window.innerWidth + x / 20
+						: pos[i][0] * window.innerWidth - x / 20) + 'px';
+
+				style.marginTop =
+					([2, 4, 7, 9, 10, 11, 15, 16, 17, 19, 22].indexOf(i) !== -1
+						? pos[i][1] * window.innerHeight + y / 20
+						: pos[i][1] * window.innerHeight - y / 20) + 'px';
+
+				style.position = 'absolute';
+			}
+		});
+	};
+
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/skills' element={<Skills />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/skills' element={<Skills />} />
+				</Routes>
+			</BrowserRouter>
+			<div id='langs'>
+				{/* <p className='scribble' id='React'>
+			React
+		</p>
+		<p className='scribble' id='GraphQL'>
+			GraphQl
+		</p>
+		<p className='scribble' id='CSharp'>
+			C#
+		</p>
+		<p className='scribble' id='CSharp1'>
+			C#1
+		</p>
+		<p className='scribble' id='CSharp2'>
+			C#2
+		</p>
+		<p className='scribble' id='CSharp3'>
+			C#3
+		</p>
+		<p className='scribble' id='CSharp4'>
+			C#4
+		</p>
+		<p className='scribble' id='CSharp5'>
+			C#5
+		</p>
+		<p className='scribble' id='CSharp6'>
+			C#6
+		</p>
+		<p className='scribble' id='CSharp7'>
+			C#7
+		</p>
+		<p className='scribble' id='CSharp8'>
+			C#8
+		</p>
+		<p className='scribble' id='CSharp9'>
+			C#9
+		</p> */}
+			</div>
+		</>
 	);
 }
 
--- src/pages/aboutPage.tsx
+++ src/pages/aboutPage.tsx
@@ -1,161 +1,53 @@
-import React, { useEffect } from "react";
-//import { Link } from 'react-router-dom';
-import axios from "axios";
-import picture from "../nonbinary-person.png";
-import { Link } from "react-router-dom";
+import picture from '../nonbinary-person.png';
+import { Link } from 'react-router-dom';
 
 function About() {
-  //var desc;
-  var lang: string[] = [];
-  var pos = [
-    [100, 100],
-    [1236, 640],
-    [170, 540],
-    [1264, 125],
-    [628, 100],
-    [565, 486],
-    [265, 345],
-    [413, 629],
-    [1186, 397],
-    [853, 579],
-    [218, 130],
-    [886, 397],
-  ];
-
-  // const axiosInstance = axios.create();
-  // axiosInstance
-  // 	.get("page/1")
-  // 	.then((response) => {
-  // 		desc = response.data;
-  // 	})
-  // 	.catch(console.error);
-
-  useEffect(() => {
-    const axiosInstance = axios.create();
-    axiosInstance
-      .get("https://localhost:7062/api/Skills")
-      .then((response) => {
-        response.data.forEach((elem: any) => {
-          var p = document.createElement("p");
-          p.className = "scribble";
-          p.id = elem.skillName;
-          p.innerHTML = elem.skillName;
-          p.style.marginLeft = pos[elem.skillID - 1][0] + "px";
-          p.style.marginTop = pos[elem.skillID - 1][1] + "px";
-          p.style.position = "absolute";
-          document.getElementById("langs")?.appendChild(p);
-          lang.push(elem.skillName);
-        });
-      })
-      .catch(console.error);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  document.onmousemove = function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    lang.forEach((entry, i) => {
-      var style = document.getElementById(entry)?.style;
-      if (style != null) {
-        style.marginLeft =
-          ([1, 4, 5, 8, 10, 14, 15, 16, 19, 21].indexOf(i) !== -1
-            ? pos[i][0] + x / 20
-            : pos[i][0] - x / 20) + "px";
-
-        style.marginTop =
-          ([2, 4, 7, 9, 10, 11, 15, 16, 17, 19, 22].indexOf(i) !== -1
-            ? pos[i][1] + y / 20
-            : pos[i][1] - y / 20) + "px";
-
-        style.position = "absolute";
-      }
-    });
-  };
-
-  return (
-    <>
-      <div id="hej">
-        <div id="about">
-          <div id="title">
-            <h1>About me</h1>
-            <Link to="/">
-              <button className="button button1">HomePage</button>
-            </Link>
-          </div>
-          <div id="content">
-            <div id="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              porro minima delectus. Cupiditate rem ducimus non voluptatem in.
-              Pariatur enim quae perspiciatis dolores veritatis, tempore modi
-              similique quisquam error ad aspernatur obcaecati voluptatibus
-              incidunt, quis quos delectus aut quibusdam. Dignissimos recusandae
-              aspernatur ea facilis fugiat tempore expedita explicabo nesciunt
-              officiis aliquid libero corrupti veniam fuga nisi harum, possimus
-              iusto est voluptate voluptas quibusdam repellendus. Iusto
-              aspernatur quis nisi rerum ea quae id, dolorem voluptatum quasi
-              ipsum ratione corporis ipsa dignissimos praesentium molestiae sed
-              neque hic tempora provident debitis nemo odit sit sequi!
-              Reprehenderit exercitationem quidem voluptatibus blanditiis
-              facilis quisquam tenetur.
-            </div>
-            <div id="rightSide">
-              <div id="picture">
-                <img id="pic" src={picture} alt="insertPersonHere" />
-              </div>
-              <div id="list">
-                <ul>
-                  <li className="item1">1</li>
-                  <li className="item2">2</li>
-                  <li className="item3">3</li>
-                  <li className="item4">4</li>
-                  <li className="item5">5</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div id="floats"></div>
-        </div>
-      </div>
-      <div id="langs">
-        {/* <p className='scribble' id='React'>
-					React
-				</p>
-				<p className='scribble' id='GraphQL'>
-					GraphQl
-				</p>
-				<p className='scribble' id='CSharp'>
-					C#
-				</p>
-				<p className='scribble' id='CSharp1'>
-					C#1
-				</p>
-				<p className='scribble' id='CSharp2'>
-					C#2
-				</p>
-				<p className='scribble' id='CSharp3'>
-					C#3
-				</p>
-				<p className='scribble' id='CSharp4'>
-					C#4
-				</p>
-				<p className='scribble' id='CSharp5'>
-					C#5
-				</p>
-				<p className='scribble' id='CSharp6'>
-					C#6
-				</p>
-				<p className='scribble' id='CSharp7'>
-					C#7
-				</p>
-				<p className='scribble' id='CSharp8'>
-					C#8
-				</p>
-				<p className='scribble' id='CSharp9'>
-					C#9
-				</p> */}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div id='hej'>
+				<div id='about'>
+					<div id='title'>
+						<h1>About me</h1>
+						<Link to='/'>
+							<button className='button button1'>HomePage</button>
+						</Link>
+					</div>
+					<div id='content'>
+						<div id='description'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+							porro minima delectus. Cupiditate rem ducimus non voluptatem in.
+							Pariatur enim quae perspiciatis dolores veritatis, tempore modi
+							similique quisquam error ad aspernatur obcaecati voluptatibus
+							incidunt, quis quos delectus aut quibusdam. Dignissimos recusandae
+							aspernatur ea facilis fugiat tempore expedita explicabo nesciunt
+							officiis aliquid libero corrupti veniam fuga nisi harum, possimus
+							iusto est voluptate voluptas quibusdam repellendus. Iusto
+							aspernatur quis nisi rerum ea quae id, dolorem voluptatum quasi
+							ipsum ratione corporis ipsa dignissimos praesentium molestiae sed
+							neque hic tempora provident debitis nemo odit sit sequi!
+							Reprehenderit exercitationem quidem voluptatibus blanditiis
+							facilis quisquam tenetur.
+						</div>
+						<div id='rightSide'>
+							<div id='picture'>
+								<img id='pic' src={picture} alt='insertPersonHere' />
+							</div>
+							<div id='list'>
+								<ul>
+									<li className='item1'>1</li>
+									<li className='item2'>2</li>
+									<li className='item3'>3</li>
+									<li className='item4'>4</li>
+									<li className='item5'>5</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div id='floats'></div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default About;
